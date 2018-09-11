f34585a6044b7008360fcba7c0c91f3095fd8bd3
CCNE <truongshinigami>
2018-09-11 23:41:01 +0700

    change a bit

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -1,11 +1,46 @@
 package week1;
+import.java.util.*;
 
 public class Student {
 
     // TODO: khai báo các thuộc tính cho Student
-
+    private String name;
+    private String id;
+    private String group;
+    private String email;
     // TODO: khai báo các phương thức getter, setter cho Student
 
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String n){
+        n = getName();
+    }
+
+    public String getID(){
+        return id;
+    }
+
+    public void setID(String n){
+        n = getID();
+    }
+
+    public String getGroup(){
+        return group;
+    }
+
+    public void setGroup(String n){
+        n = getGroup();
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String n){
+        n = getEmail();
+    }
     /**
      * Constructor 1
      */
