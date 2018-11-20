25fdecac8ee83ed8f51249e34405bf1e4a340228
truongshinigami <37414642+truongshinigami@users.noreply.github.com>
2018-11-21 01:16:41 +0700

    1234

--- src/week11/Task1.java
+++ src/week11/Task1.java
@@ -1,4 +1,44 @@
 package week11;
 
+import java.util.Arrays;
+
 public class Task1 {
+    private <E> void swap ( E [] a,int i,int j) {
+        E temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+    public <E extends Comparable<E>> void sorting(E [] a){
+        for (int i = 0;i< a.length - 1;i++){
+            // tìm phần tử nhỏ nhất
+            int smallest = i;
+            for (int j = i+1;j<a.length;j++){
+                // compareTo trả về giá trị nhỏ hơn 0 khi a[j] > a[smallest]
+                if (a[j].compareTo(a[smallest]) <= 0)
+                    smallest = j;
+            }
+            // xếp phần tử nhỏ nhất lên đầu.
+            swap (a,i,smallest);
+        }
+
+
+    }
+    public static void main(String [] abc){
+        Task1 test = new Task1();
+        Integer [] arr1 = {10,6,9,7,4,3};
+        String [] arr2 = {"abc","efg","tdg","dwa", "weg"};
+        Character [] arr3 = {'a','b','c','d'};
+
+        System.out.println("List before sort : " + Arrays.toString(arr1) );
+        System.out.println("List before sort : " + Arrays.toString(arr2) );
+        System.out.println("List before sort : " + Arrays.toString(arr3) );
+
+        test.sorting(arr1);
+        test.sorting(arr2);
+        test.sorting(arr3);
+
+        System.out.println("List after sort : " + Arrays.toString(arr1));
+        System.out.println("List after sort : " + Arrays.toString(arr2));
+        System.out.println("List after sort : " + Arrays.toString(arr3));
+    }
 }
--- src/week11/Task2.java
+++ src/week11/Task2.java
@@ -1,4 +1,22 @@
 package week11;
 
 public class Task2 {
+    public  <E extends Comparable<E>> int max(E[] a){
+        int maxIndex = 0;
+        for (int i = 1;i<a.length;i++)
+            // phan tu tai vi tri maxIndex nho hon phan tu tai vi tri i
+            if (a[maxIndex].compareTo(a[i]) <0)
+                maxIndex = i;
+    return maxIndex;
+    }
+    public static void main(String [] abc){
+        Task2 findmax = new Task2();
+        Integer [] arr1 = {10,6,9,7,4,3};
+        String [] arr2 = {"abc","efg","tdg","dwa", "weg"};
+        Character [] arr3 = {'a','b','c','d'};
+
+        System.out.println("Max of Integer : " + arr1[findmax.max(arr1)]);
+        System.out.println("Max of String : " + arr2[findmax.max(arr2)]);
+        System.out.println("Max of Character : " + arr3[findmax.max(arr3)]);
+    }
 }
